2e4ff42cfc37173a3a14146ec3575498d53a30df
Csabi <glinto@gmail.com>
2015-03-18 14:19:04 +0100

    Support for mocked/live content

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(-1);
 include('slack.php');
 include('Project.php');
 include('User.php');
 include('Event.php');
 
 require_once('config.php');
-require_once('DB.php');
 
 if ($prod) {
+	require_once('DB.php');
 	DB::init($dbconfig);
+	Page::$mocked = false;
+}
+else
+{
+	Page::$mocked = true;
 }
 
 class Page
 {
 	public static $title;
+	public static $mocked;
 
 	public static function getCommand()
 	{
@@ -25,20 +31,30 @@ class Page
 
 	public static function renderProject($name)
 	{
-		//$p = Project::load($name);
-		$p = new Project('Slack workgroup tool', 'A tool for mapping project members and focus', 'syntaxerror'); 
+		if (Page::$mocked)
+			$p = new Project('Slack workgroup tool', 'A tool for mapping project members and focus', 'syntaxerror'); 
+		else
+			$p = Project::load($name);
 		if ($p == null) return '';
+
 		$output = '<div class="project"><h1>'.$p->name.'</h1><span class="small">by @'.
 			$p->owner.'</span><p>'.$p->desc.'</p></div>';
-		//$members = $p->getMembers();
-		//$logs = $p->getLog();
-		$members = array(
-			array('name' => 'syntaxerror', 'focus' => true),
-			array('name' => 'csabi', 'focus' => false),
-			);
-		$logs = array(new Event('2015-01-01', 'Whatever', 'syntaxerror', 'joined the project.'),
-			new Event('2015-01-01', 'Whatever', 'csabi', 'left the project.'),	
-			);
+		
+		if (Page::$mocked)
+			$members = array(
+				array('name' => 'syntaxerror', 'focus' => true),
+				array('name' => 'csabi', 'focus' => false),
+				);
+		else
+			$members = $p->getMembers();
+		
+		if (Page::$mocked)
+			$logs = array(new Event('2015-01-01', 'Whatever', 'syntaxerror', 'joined the project.'),
+				new Event('2015-01-01', 'Whatever', 'csabi', 'left the project.'),	
+				);
+		else
+			$logs = $p->getLog();
+
 		$output .= '<h2>Members</h2>';
 		foreach ($members as $m)
 		{
@@ -47,7 +63,7 @@ class Page
 		$output .= '<h2>Log</h2>';
 		foreach ($logs as $l)
 		{
-			$output .= '<div class="log"><span class="small">'.$l->time.' @<a href="index.php?q=user/'.$l->user.'">'.$l->user.'</a></span><br />'.$l->message.'</div>';
+			$output .= '<div class="log"><span class="small">'.$l->time.' <a href="index.php?q=user/'.$l->user.'">'.$l->user.'</a></span><br />'.$l->message.'</div>';
 		}
 		return $output;
 	}
@@ -55,11 +71,14 @@ class Page
 
 	public static function renderProjects()
 	{
-		$projects = array(new Project('Slack workgroup tool', 'A tool for mapping project members and focus', 'syntaxerror'),
-			new Project('PjM job description', 'Have a document about their roles', 'csabi'),
-		);
-		//Project:listAll();
-		$output = '';
+		if (Page::$mocked)
+			$projects = array(new Project('Slack workgroup tool', 'A tool for mapping project members and focus', 'syntaxerror'),
+				new Project('PjM job description', 'Have a document about their roles', 'csabi'),
+			);
+		else
+			$projects = Project::listAll();
+		
+		$output = '<h1>Projects</h1>';
 		foreach ($projects as $p)
 		{
 			$output .= '<div class="project"><a href="index.php?q=project/'.$p->name.'">'.$p->name.'</a><br/><span class="small">by @'.
@@ -71,16 +90,23 @@ class Page
 
 	public static function renderUser($name)
 	{
-		//$u = User:load($name);
-		$u = new User('csabi');	
+		if (Page::$mocked)
+			$u = new User('csabi');	
+		else
+			$u = User::load($name);
+
 		if (!$u) return '';
 		$output = '<div class="user"><h1>'.$u->name.'</h1></div>';
 		$output .= '<h2>Projects</h2>';
-		$projects = $u->getProjects();
-		$projects = array(
-			array("name" => "Consul", "focus" => true),
-			array("name" => "Logging", "focus" => false)
-			);
+
+		if (Page::$mocked)
+			$projects = array(
+				array("name" => "Consul", "focus" => true),
+				array("name" => "Logging", "focus" => false)
+				);
+		else
+			$projects = $u->getProjects();
+
 		foreach ($projects as $p)
 		{
 			$output .= '<div class="project"><a href="index.php?q=project/'.$p['name'].'">'.$p['name'].'</a> '.($p['focus'] ? '[x]' : '').'</div>';
@@ -90,9 +116,12 @@ class Page
 
 	public static function renderUsers()
 	{
-		$users = array(new User('csabi'), new User('syntaxerror'));
-		//User::listAll();
-		$output = '';
+		if (Page::$mocked)
+			$users = array(new User('csabi'), new User('syntaxerror'));
+		else
+			User::listAll();
+
+		$output = '<h1>Users</h1>';
 		foreach ($users as $u)
 		{
 			$output .= '<div class="user"><a href="/index.php?q=user/'.$u->name.'">'.$u->name.'</a></div>';
